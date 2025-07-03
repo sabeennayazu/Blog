fc843b880555118bc4537403807e53503f74bb69
sabeen nayazu <sabeennayazu000@gmail.com>
2025-07-03 11:13:52 +0545

    'okoko'

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,4 +3,7 @@ from blog_app import views
 
 urlpatterns = [
     path('', views.post_list, name='post-list'),
+    path('detail/<int:pk>/', views.post_detail, name='post-detail'),
+    path('draft-list/', views.draft_list, name='draft-list'),
+    path('draft-detail/<int:pk>/', views.draft_detail, name='draft-detail'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
-from .models import Post  # Import your Post model
+from blog_app.models import Post
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all()  # Assuming you have a Post model
-    return render(request, 'post_list.html', {'posts': posts})  # context , dictionary to pass data to the template
+    posts=Post.objects.filter(published_at__isnull=False).order_by("-published_at")
+    return render(
+        request,
+        "post_list.html",
+        {"posts":posts},  #CONTEXT =>needed datas 
+    )
+# Create your views here.
+def post_detail(request,pk):
+    post=Post.objects.get(pk=pk, published_at__isnull=False)
+    return render(
+        request,
+        "post_detail.html",
+        {"post": post},
+    )
+@login_required
+def draft_list(request):
+    posts = Post.objects.filter(published_at__isnull=True)
+    return render(
+        request,
+        "draft_list.html",
+        {"posts": posts},
+    )
+@login_required
+def draft_detail(request, pk):
+    post = Post.objects.get(pk=pk, published_at__isnull=True)
+    return render(
+        request,
+        "draft_detail.html",
+        {"post": post},
+    )
